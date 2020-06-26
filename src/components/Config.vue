9a846ff761bf5aa964e60eaa78c3f8558d96fb79
Michal Gil <michalgil@pryvmbp.home>
2020-06-26 15:45:56 +0200

    1st attempts

---
+++ src/components/Config.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="config">
+    <h1>{{ msg }}</h1>
+    <h3>H3</h3>
+    <div>
+      <span v-for="n in 10" :key="n"> {{ n }} </span>
+    </div>
+    <ul>
+      <li v-for="el in list1" :key="el.val">
+        {{ el }}
+      </li>
+    </ul>
+    <ul>
+      <li v-for="todo in todos" :key="todo">
+        {{ todo }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Config",
+  props: {
+    msg: String
+  }
+  ,
+  data: () => ({
+    todos: [1,2,3],
+    list1: [
+      {
+        val: 'xoxo'
+      },
+      {
+        val: 'xxop'
+      }
+    ]
+  })
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Config msg="PryV Configuration Panel" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Config from "@/components/Config.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    Config
   }
 };
 </script>
