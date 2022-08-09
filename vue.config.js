6d79314a3d1ef928f8fa5cf1b581e3c2a2504f40
Simon Goumaz <simon@attentif.ch>
2022-08-09 17:09:00 +0200

    Replace `yarn` with `just` (WIP)

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
-const recLaOptions = require('rec-la').httpsOptions;
+const recLaOptions = require('rec.la').httpsOptions();
 recLaOptions.https = true;
 recLaOptions.host = 'l.rec.la';
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '' : '/',
-  devServer: recLaOptions,
+  devServer: recLaOptions
 };
